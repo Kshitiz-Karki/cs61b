d8dc09e8cfe3a92587511d97a234f865a4a13e60
Kshitiz Karki <karki.kshitiz.23@gmail.com>
2024-08-08 15:10:44 +0545

    remove print statements

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -24,25 +24,6 @@ public class TestBuggyAList {
       assertEquals(arrNoResize.removeLast(), buggyAList.removeLast());
       assertEquals(arrNoResize.removeLast(), buggyAList.removeLast());
       assertEquals(arrNoResize.removeLast(), buggyAList.removeLast());
-
-//      int i = 1;
-//      while (i <= 17) {
-//        buggyAList.addLast(i);
-//        arrNoResize.addLast(i);
-//        i++;
-//      }
-//    //items.length = 32
-//      assertEquals(buggyAList.size(), 17);
-//      assertEquals(arrNoResize.size(), 17);
-//      int j = 1;
-//      while (j <= 11) {
-//        buggyAList.removeLast();
-//        arrNoResize.removeLast();
-//        j++;
-//      }
-//      assertEquals(buggyAList.size(), 6);
-//      assertEquals(arrNoResize.size(), 6);
-//      assertEquals(arrNoResize.getLast(), buggyAList.getLast());
   }
 
   @Test
@@ -57,23 +38,16 @@ public class TestBuggyAList {
         int randVal = StdRandom.uniform(0, 100);
         L.addLast(randVal);
         B.addLast(randVal);
-        System.out.println("L.addLast(" + randVal + ")");
-        System.out.println("B.addLast(" + randVal + ")");
       } else if (operationNumber == 1) {
         // size
         int size1 = L.size();
         int size2 = B.size();
-        assertEquals(size1, size2);
-        System.out.println("size1: " + size1);
-        System.out.println("size2: " + size2);
         assertEquals(size1, size2);
       } else if (operationNumber == 2) {
         //getLast
         if (L.size() > 0 && B.size() > 0) {
           int lLast = L.getLast();
           int bLast = B.getLast();
-          System.out.println("L.getLast(" + lLast + ")");
-          System.out.println("B.getLast(" + bLast + ")");
           assertEquals(lLast, bLast);
         }
       } else if (operationNumber == 3) {
@@ -81,8 +55,6 @@ public class TestBuggyAList {
         if (L.size() > 0 && B.size() > 0) {
           int x = L.removeLast();
           int y = B.removeLast();
-          System.out.println("L.removeLast(" + x + ")");
-          System.out.println("B.removeLast(" + y + ")");
           assertEquals(x, y);
         }
       }
